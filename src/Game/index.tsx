92fc761eddfe6748ac61a75fb73f61f034c74a19
patrickburin <patrickburin17@gmail.com>
2023-11-21 23:09:38 -0300

    integração teste

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -17,6 +17,7 @@ import "react-toastify/dist/ReactToastify.css";
 //react
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 //react icons
 import { FaQuestion } from "react-icons/fa6";
@@ -30,82 +31,33 @@ const Game: React.FC = () => {
     navigate("/");
   };
 
-  const flagsData = [
-    { name: "Bandeira 1", isCorrect: true },
-    { name: "Bandeira 2", isCorrect: false },
-    { name: "Bandeira 3", isCorrect: false },
-    { name: "Bandeira 4", isCorrect: false },
-  ];
-
-  const shuffleFlags = (flags: any) => {
-    return flags.sort(() => Math.random() - 0.5);
-  };
-
   useEffect(() => {
-    // Lógica para buscar as bandeiras do backend
-    // Suponhamos que você fez uma requisição e recebeu flagsData do backend
-    const shuffledFlags = shuffleFlags(flagsData);
-    setFlags(shuffledFlags);
-  }, []);
+    axios.get("http://localhost:8080/getOptions").then((response) => {
+      console.log(response);
+      console.log("estou aqui");
+      console.log(response.data);
+    });
+  }, [flags]);
+
+  // const flagsData = [
+  //   { name: "Bandeira 1", isCorrect: true },
+  //   { name: "Bandeira 2", isCorrect: false },
+  //   { name: "Bandeira 3", isCorrect: false },
+  //   { name: "Bandeira 4", isCorrect: false },
+  // ];
 
   // const sendAnswerToBackend = async (isCorrect: boolean) => {
-  //   try {
-  //     const response = await new Promise((resolve) => {
-  //       // Simulação de requisição ao backend com atraso de 2 segundos
-  //       setTimeout(() => {
-  //         // Aqui normalmente faria a requisição ao backend
-  //         // Simulando uma resposta do backend
-  //         const backendResponse = { success: true }; // Simulação de uma resposta de sucesso
-
-  //         resolve(backendResponse);
-  //       }, 2000);
-  //     });
-
-  //     // Aqui você poderia processar a resposta do backend, se necessário
-  //     console.log("Resposta do backend:", response);
-
-  //     return response; // Retornar a resposta do backend se for útil
-  //   } catch (error) {
-  //     console.error("Erro ao enviar resposta ao backend:", error);
-  //     throw error;
-  //   }
+  //   return new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       const backendResponse = { success: true }; // Simulação de uma resposta de sucesso
+  //       resolve(backendResponse);
+  //     }, 1000);
+  //   });
   // };
-
-  const fetchFlagsFromBackend = async () => {
-    return new Promise((resolve) => {
-      // Simulação de requisição ao backend com atraso de 2 segundos
-      setTimeout(() => {
-        const flagsFromBackend = [
-          { name: "Bandeira 1", isCorrect: true },
-          { name: "Bandeira 2", isCorrect: false },
-          { name: "Bandeira 3", isCorrect: false },
-          { name: "Bandeira 4", isCorrect: false },
-        ];
-        resolve(flagsFromBackend);
-      }, 2000);
-    });
-  };
-
-  const sendAnswerToBackend = async (isCorrect: boolean) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const backendResponse = { success: true }; // Simulação de uma resposta de sucesso
-        resolve(backendResponse);
-      }, 1000);
-    });
-  };
 
   const handleOptionClick = async (isCorrect: boolean) => {
     if (isCorrect) {
-      try {
-        const response = await sendAnswerToBackend(isCorrect);
-        toast.success("Resposta correta!");
-        const shuffledFlags = shuffleFlags(flagsData);
-        setFlags(shuffledFlags);
-      } catch (error) {
-        // Tratar erros se a requisição ao backend falhar
-        console.error("Erro ao enviar resposta correta ao backend:", error);
-      }
+      toast.success("Acertou!");
     } else {
       toast.error("Errou! Tente novamente.");
     }
@@ -130,7 +82,19 @@ const Game: React.FC = () => {
           </div>
         </S.Flag>
         <S.Options>
-          {flags.map((flag, index) => (
+          <button
+            onClick={() => {
+              setFlags([
+                { name: "Bandeira 1", isCorrect: true },
+                { name: "Bandeira 2", isCorrect: false },
+                { name: "Bandeira 3", isCorrect: false },
+                { name: "Bandeira 4", isCorrect: false },
+              ]);
+            }}
+          >
+            aaaaaaaaaa
+          </button>
+          {/* {flags.map((flag, index) => (
             <div className="divider" key={index}>
               <div
                 className="option"
@@ -139,7 +103,7 @@ const Game: React.FC = () => {
                 {flag.name}
               </div>
             </div>
-          ))}
+          ))} */}
         </S.Options>
       </S.Content>
       <Toastify />
